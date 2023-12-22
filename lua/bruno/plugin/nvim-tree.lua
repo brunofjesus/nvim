e5c404a249ec997a94873a330b9ff17d0ed8151b
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-12-22 18:28:55 +0000

    Stealing nvim-tree config from nvchad

--- lua/bruno/plugin/nvim-tree.lua
+++ lua/bruno/plugin/nvim-tree.lua
@@ -9,38 +9,90 @@ return {
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
-		
+
 		-- change color for arrows in tree to light blue
 		vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-		
+
 		-- configure nvim-tree
 		nvimtree.setup({
-		  -- change folder arrow icons
-		  renderer = {
-		    icons = {
-		      glyphs = {
-		        folder = {
-		          --arrow_closed = "", -- arrow when folder is closed
-		          --arrow_open = "", -- arrow when folder is open
-		        },
-		      },
-		    },
-		  },
-		  -- disable window_picker for
-		  -- explorer to work well with
-		  -- window splits
-		  actions = {
-		    open_file = {
-		      window_picker = {
-		        enable = false,
-		      },
-		    },
-		  },
-		  -- 	git = {
-		  -- 		ignore = false,
-		  -- 	},
+      filters = {
+        dotfiles = false,
+        exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
+      },
+      disable_netrw = true,
+      hijack_netrw = true,
+      hijack_cursor = true,
+      hijack_unnamed_buffer_when_opening = false,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        adaptive_size = false,
+        side = "left",
+        width = 30,
+        preserve_window_proportions = true,
+      },
+      git = {
+        enable = false,
+        ignore = true,
+      },
+      filesystem_watchers = {
+        enable = true,
+      },
+      actions = {
+        open_file = {
+	    	  window_picker = {
+	    	    enable = false,
+	    	  },
+          resize_window = true,
+        },
+      },
+      renderer = {
+        root_folder_label = false,
+        highlight_git = false,
+        highlight_opened_files = "none",
+
+        indent_markers = {
+          enable = false,
+        },
+
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+          },
+
+          glyphs = {
+            default = "󰈚",
+            symlink = "",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+              symlink_open = "",
+              arrow_open = "",
+              arrow_closed = "",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
 		})
-		
+
 		local keymap = vim.keymap
 		keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
 		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>")
