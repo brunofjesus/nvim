deb1f98bd4b096ac5fccaa68b378e8f55c285151
Bruno Jesus <bruno.fl.jesus@gmail.com>
2024-09-13 15:29:22 +0100

    replace tsserver with ts_ls

--- lua/bruno/plugin/lsp/mason.lua
+++ lua/bruno/plugin/lsp/mason.lua
@@ -25,7 +25,7 @@ return {
       -- list of servers for mason to install
       ensure_installed = {
         -- web development
-        "tsserver",
+        "ts_ls",
         "html",
         "cssls",
         "lua_ls",
