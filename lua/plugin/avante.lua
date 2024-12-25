da7fa4a78ad50feb5cdc708d44380f60fbb417bf
Bruno Jesus <bruno.fl.jesus@gmail.com>
2024-12-25 22:42:48 +0000

    Add toggle markdown renderer

--- lua/plugin/avante.lua
+++ lua/plugin/avante.lua
@@ -16,7 +16,7 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     --"zbirenbaum/copilot.lua", -- for providers='copilot'
     {
@@ -43,6 +43,9 @@ return {
         file_types = { "markdown", "Avante" },
       },
       ft = { "markdown", "Avante" },
+      keys = {
+        { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
+      },
     },
   },
 }
