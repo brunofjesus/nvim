cac6729fe5abc05833283999856423299f3887d1
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-03-20 09:51:14 +0000

    Add ansible vault encrypt and decrypt commands

--- lua/core/commandalias.lua
+++ lua/core/commandalias.lua
@@ -1,1 +1,3 @@
 vim.api.nvim_create_user_command('Prettier', ":%!npx prettier --stdin-filepath %", { desc = "Format with prettier" })
+vim.api.nvim_create_user_command('AnsibleVaultDecrypt', "!ansible-vault decrypt --vault-password-file=~/.vault_pass %", { desc = "Decrypt ansible vault file" })
+vim.api.nvim_create_user_command('AnsibleVaultEncrypt', "!ansible-vault encrypt --vault-password-file=~/.vault_pass %", { desc = "Encrypt ansible vault file" })
