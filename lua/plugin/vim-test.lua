8adc29e632c9423b21fc0ba04e0972ed7a217a45
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-10-05 00:26:20 +0100

    set vim-test tmux strategy only on tmux

--- lua/plugin/vim-test.lua
+++ lua/plugin/vim-test.lua
@@ -1,12 +1,18 @@
 return {
 	"vim-test/vim-test",
-  dependencies =  {
-    "preservim/vimux",
-  },
-	vim.keymap.set("n", "<leader>dgn", ":TestNearest<CR>"),
-	vim.keymap.set("n", "<leader>dgf", ":TestFile<CR>"),
-	vim.keymap.set("n", "<leader>dgs", ":TestSuite<CR>"),
-	vim.keymap.set("n", "<leader>dgl", ":TestLast<CR>"),
-	vim.keymap.set("n", "<leader>dgv", ":TestVisit<CR>"),
-  vim.cmd("let test#strategy = \"vimux\"")
+	dependencies = {
+		"preservim/vimux",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>dgn", ":TestNearest<CR>")
+		vim.keymap.set("n", "<leader>dgf", ":TestFile<CR>")
+		vim.keymap.set("n", "<leader>dgs", ":TestSuite<CR>")
+		vim.keymap.set("n", "<leader>dgl", ":TestLast<CR>")
+		vim.keymap.set("n", "<leader>dgv", ":TestVisit<CR>")
+
+    local in_tmux = vim.env.TMUX ~= nil
+    if in_tmux then
+		  vim.cmd('let test#strategy = "vimux"')
+    end
+	end,
 }
