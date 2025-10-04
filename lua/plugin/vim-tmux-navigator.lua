3557c027c9a374f3438f0a386642097632b90603
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-10-04 23:48:29 +0100

    add tmux navigation

---
+++ lua/plugin/vim-tmux-navigator.lua
@@ -0,0 +1,19 @@
+local in_tmux = vim.env.TMUX ~= nil
+return {
+  "christoomey/vim-tmux-navigator",
+  enabled = in_tmux;
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+  },
+}
