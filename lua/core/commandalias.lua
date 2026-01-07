4c9587fd28f9dbee8b68725933ec80cb33889074
Bruno Jesus <bruno.fl.jesus@gmail.com>
2026-01-07 14:06:00 +0000

    preserve cursor and folds when running :Prettier

--- lua/core/commandalias.lua
+++ lua/core/commandalias.lua
@@ -1,3 +1,16 @@
-vim.api.nvim_create_user_command('Prettier', ":%!npx prettier --stdin-filepath %", { desc = "Format with prettier" })
-vim.api.nvim_create_user_command('AnsibleVaultDecrypt', "!ansible-vault decrypt --vault-password-file=~/.vault_pass %", { desc = "Decrypt ansible vault file" })
-vim.api.nvim_create_user_command('AnsibleVaultEncrypt', "!ansible-vault encrypt --vault-password-file=~/.vault_pass %", { desc = "Encrypt ansible vault file" })
+vim.api.nvim_create_user_command("Prettier", function()
+	local view = vim.fn.winsaveview()
+	vim.cmd(":%!npx prettier --stdin-filepath " .. vim.fn.expand("%"))
+	vim.fn.winrestview(view)
+end, { desc = "Format with prettier" })
+
+vim.api.nvim_create_user_command(
+	"AnsibleVaultDecrypt",
+	"!ansible-vault decrypt --vault-password-file=~/.vault_pass %",
+	{ desc = "Decrypt ansible vault file" }
+)
+vim.api.nvim_create_user_command(
+	"AnsibleVaultEncrypt",
+	"!ansible-vault encrypt --vault-password-file=~/.vault_pass %",
+	{ desc = "Encrypt ansible vault file" }
+)
