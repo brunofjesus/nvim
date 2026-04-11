ff0842525527f75e8143fce6eeda5d7aff7350eb
Bruno Jesus <bruno.fl.jesus@gmail.com>
2026-04-11 20:13:29 +0100

    send osc11 query on start

--- lua/plugin/osc11.lua
+++ lua/plugin/osc11.lua
@@ -1,15 +1,20 @@
 local env = require("utils.env")
 return {
   "afonsofrancof/OSC11.nvim",
-  enabled = env.should_load.osc11;
+  enabled = env.should_load.osc11,
   opts = {
-    -- Function to call when switching to dark theme
     on_dark = function()
       vim.cmd("EnableDarkMode")
     end,
-    -- Function to call when switching to light theme
     on_light = function()
       vim.cmd("DisableDarkMode")
     end,
-  }
+  },
+  config = function(_, opts)
+    require("osc11").setup(opts)
+    vim.defer_fn(function()
+      io.stdout:write("\027]11;?\007")
+      io.stdout:flush()
+    end, 100)
+  end,
 }
