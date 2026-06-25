f3f498a5d1346f5ca0c003f69e8c9223e12b6345
Bruno Jesus <bruno.fl.jesus@gmail.com>
2026-06-25 10:38:14 +0100

    replace <leader>mt with <leader>umr and <leader>umt

--- lua/core/markdown_toggle.lua
+++ lua/core/markdown_toggle.lua
@@ -1,16 +1,9 @@
-vim.api.nvim_create_user_command(
-  'MarkdownToggle',
-  function()
-    vim.cmd('RenderMarkdown buf_toggle')      -- render-markdown.nvim, current buffer
-    vim.cmd('MarkdownTableToggleAutoPreview')  -- markdown-table-wrap.nvim, current buffer
-  end,
-  { desc = 'Toggle markdown rendering (render-markdown + table-wrap) for current buffer' }
-)
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'markdown',
   callback = function(args)
-    vim.keymap.set('n', '<leader>mt', '<cmd>MarkdownToggle<cr>',
-      { buffer = args.buf, desc = 'Toggle markdown rendering' })
+    vim.keymap.set('n', '<leader>umr', '<cmd>RenderMarkdown buf_toggle<cr>',
+      { buffer = args.buf, desc = 'Toggle render-markdown for current buffer' })
+    vim.keymap.set('n', '<leader>umt', '<cmd>MarkdownTableToggleAutoPreview<cr>',
+      { buffer = args.buf, desc = 'Toggle markdown table auto preview' })
   end,
 })
